73b1f42bf05df5a196b36a9da71781862bc1424e
Son Nguyen <95949597+sonnguyen1510@users.noreply.github.com>
2023-11-01 17:02:31 +0700

    v1.0
    add bottom navigation

---
+++ FoodDelivery/FoodDelivery/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodDelivery
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FoodDelivery/FoodDelivery/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FoodDelivery
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
