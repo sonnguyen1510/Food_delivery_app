fae1b89b4f5b4beb96550fc21733c0b2784f8157
Son Nguyen <95949597+sonnguyen1510@users.noreply.github.com>
2023-11-06 08:56:23 +0700

    v1.1.3
    Update project name & solotion to MoneyManager

---
+++ FoodDelivery/MoneyManager/Database/Server/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyManager.Database.Server.Model;
+using System.Collections.Generic;
+
+namespace MoneyManager.Database.Server
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserTransaction> UserTransactions { get; set; }
+    }
+}
---
+++ FoodDelivery/MoneyManager/Database/Server/Model/UserTransactionService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyManager.Database.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManager.Database.Server.Model
+{
+    public class UserTransactionService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserTransactionService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<UserTransaction>> getAllTransactions() => await _context.UserTransactions.ToListAsync();
+    }
+}
