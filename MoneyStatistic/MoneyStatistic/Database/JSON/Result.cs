21716e6f524fd511bd0703971f83f49644838e9a
Son Nguyen <95949597+sonnguyen1510@users.noreply.github.com>
2023-11-10 16:37:59 +0700

    3.0.5
    complete add transaction layout , fix scroll view of main layout

---
+++ MoneyStatistic/MoneyStatistic/Database/JSON/Result.cs
@@ -0,0 +1,11 @@
+﻿namespace MoneyStatistic.Database.JSON
+{
+    public class Result
+    {
+
+
+        public int status { get; set; }
+        public required string message { get; set; }
+        public required string result { get; set; }
+    }
+}
